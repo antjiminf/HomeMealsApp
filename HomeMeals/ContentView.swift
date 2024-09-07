30aa68d5f5ae2f218ce691244abb62bb91e09be5
Antonio Jiménez Infante <antoniojz1998@gmail.com>
2024-09-07 18:31:33 +0200

    Initial Commit

---
+++ HomeMeals/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HomeMeals
+//
+//  Created by Antonio Jiménez Infante on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HomeMeals/HomeMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeMealsApp.swift
+//  HomeMeals
+//
+//  Created by Antonio Jiménez Infante on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
