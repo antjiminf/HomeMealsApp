2d8d70d70d250f7292f94acb48216199c6c98963
Antonio Jiménez Infante <antoniojz1998@gmail.com>
2025-03-08 02:23:23 +0100

    Secure textfields fixes and NetworkCircleImage component implemented

---
+++ HomeMeals/NetworkCircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkCircleImage.swift
+//  HomeMeals
+//
+//  Created by Antonio Jiménez Infante on 8/3/25.
+//
+
+import SwiftUI
+
+struct NetworkCircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkCircleImage()
+}
